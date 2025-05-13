1cfa9f821816ca03da982e86ad387d91651177aa
rober1o <robertoif701@gmail.com>
2025-05-13 10:04:20 -0300

    Implementação do projeto casa inteligente

---
+++ casa_inteligente.c
@@ -0,0 +1,490 @@
+
+#include "casa_inteligente.h"
+
+int main()
+{
+    inicializar_perifericos();
+    desenha_fig(matriz_apagada, BRILHO_PADRAO, pio, sm);
+
+    while (true)
+    {
+        cyw43_arch_poll();
+
+        monitorar();
+        sleep_ms(200);
+    }
+
+    cyw43_arch_deinit();
+    return 0;
+}
+
+// ********************************************* Funções *********************************************
+
+// -------------------------------------- INICIALIZAR PERIFERICOS --------------------------------------
+
+void inicializar_perifericos()
+{
+    // Inicializa a comunicação serial para depuração
+    stdio_init_all();
+
+    // Conecta ao Wi-Fi antes de inicializar os periféricos
+    conectar_wifi(); // Agora, a conexão Wi-Fi é feita aqui
+
+    // Inicializa os periféricos agora que o Wi-Fi foi estabelecido
+    inicializar_display_i2c();        // Inicializa o display I2C
+    configurar_matriz_leds();         // Configura a matriz de LEDs
+    gpio_led_bitdog();                // Configura o LED (se necessário)
+    inicializar_pwm_buzzer();         // Inicializa o PWM para o buzzer
+    atualizar_display();              // Atualiza o conteúdo do display
+    configurar_botao_bootsel();       // Configura o botão BOOTSEL
+    configurar_servidor_tcp();        // Configura o servidor TCP
+    inicializar_sensor_temperatura(); // Inicializa o sensor de temperatura
+}
+
+// Inicializar os Pinos GPIO para acionamento dos LEDs da BitDogLab
+void gpio_led_bitdog(void)
+{
+    // Configuração dos LEDs como saída
+    gpio_init(LED_BLUE_PIN);
+    gpio_set_dir(LED_BLUE_PIN, GPIO_OUT);
+    gpio_put(LED_BLUE_PIN, false);
+
+    gpio_init(LED_GREEN_PIN);
+    gpio_set_dir(LED_GREEN_PIN, GPIO_OUT);
+    gpio_put(LED_GREEN_PIN, false);
+
+    gpio_init(LED_RED_PIN);
+    gpio_set_dir(LED_RED_PIN, GPIO_OUT);
+    gpio_put(LED_RED_PIN, false);
+}
+
+void configurar_matriz_leds() // FUNÇÃO PARA CONFIGURAR O PIO PARA USAR NA MATRIZ DE LEDS
+{
+    bool clock_setado = set_sys_clock_khz(133000, false);
+    pio = pio0;
+    sm = pio_claim_unused_sm(pio, true);
+    uint offset = pio_add_program(pio, &Matriz_5x5_program);
+    Matriz_5x5_program_init(pio, sm, offset, MATRIZ_PIN);
+}
+
+void inicializar_display_i2c()
+{ // FUNÇÃO PARA INICIALIZAR O I2C DO DISPLAY
+    i2c_init(I2C_PORT, 400 * 1000);
+    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
+    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
+    gpio_pull_up(I2C_SDA);
+    gpio_pull_up(I2C_SCL);
+
+    ssd1306_init(&ssd, WIDTH, HEIGHT, false, endereco, I2C_PORT);
+    ssd1306_config(&ssd);
+    ssd1306_send_data(&ssd);
+    ssd1306_fill(&ssd, false);
+    ssd1306_send_data(&ssd);
+}
+
+void inicializar_pwm_buzzer()
+{
+    gpio_set_function(BUZZER_PIN, GPIO_FUNC_PWM);
+    uint slice_num = pwm_gpio_to_slice_num(BUZZER_PIN);
+
+    pwm_set_wrap(slice_num, 25000);                                       // Frequência: 500 Hz (125 MHz / 25000 = 500 Hz)
+    pwm_set_chan_level(slice_num, pwm_gpio_to_channel(BUZZER_PIN), 6250); // 50% duty cycle
+    pwm_set_enabled(slice_num, false);                                    // Começa desligado
+}
+
+void inicializar_sensor_temperatura()
+{
+    adc_init();
+    adc_set_temp_sensor_enabled(true);
+}
+
+void conectar_wifi()
+{
+    if (cyw43_arch_init())
+    {
+        printf("Falha ao inicializar Wi-Fi\n");
+        exit(-1);
+    }
+
+    cyw43_arch_gpio_put(LED_PIN, 0);
+    cyw43_arch_enable_sta_mode();
+
+    printf("Conectando ao Wi-Fi...\n");
+    if (cyw43_arch_wifi_connect_timeout_ms(WIFI_SSID, WIFI_PASSWORD, CYW43_AUTH_WPA2_AES_PSK, 20000))
+    {
+        printf("Falha ao conectar ao Wi-Fi\n");
+        exit(-1);
+    }
+
+    printf("Conectado ao Wi-Fi\n");
+
+    if (netif_default)
+    {
+        printf("IP do dispositivo: %s\n", ipaddr_ntoa(&netif_default->ip_addr));
+    }
+}
+
+void configurar_servidor_tcp()
+{
+    struct tcp_pcb *server = tcp_new();
+    if (!server)
+    {
+        printf("Falha ao criar servidor TCP\n");
+        exit(-1);
+    }
+
+    if (tcp_bind(server, IP_ADDR_ANY, 80) != ERR_OK)
+    {
+        printf("Falha ao associar servidor TCP à porta 80\n");
+        exit(-1);
+    }
+
+    server = tcp_listen(server);
+    tcp_accept(server, tcp_server_accept);
+    printf("Servidor ouvindo na porta 80\n");
+}
+
+void configurar_botao_bootsel()
+{
+    gpio_init(botaoB);
+    gpio_set_dir(botaoB, GPIO_IN);
+    gpio_pull_up(botaoB);
+    gpio_set_irq_enabled_with_callback(botaoB, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
+}
+
+// -------------------------------------- PROCESSAMENTO TCP --------------------------------------
+
+// Função de callback ao aceitar conexões TCP
+static err_t tcp_server_accept(void *arg, struct tcp_pcb *newpcb, err_t err)
+{
+    tcp_recv(newpcb, tcp_server_recv);
+    return ERR_OK;
+}
+
+// Tratamento do request do usuário - digite aqui
+void user_request(char **request)
+{
+
+    if (strstr(*request, "GET /luz_1") != NULL)
+    {
+        alternar_luz_1();
+    }
+    else if (strstr(*request, "GET /luz_2") != NULL)
+    {
+        alternar_luz_2();
+    }
+    else if (strstr(*request, "GET /porta") != NULL)
+    {
+        alternar_porta();
+    }
+    else if (strstr(*request, "GET /alarme") != NULL)
+    {
+        alternar_alarme();
+    }
+    else if (strstr(*request, "GET /modo_viagem") != NULL)
+    {
+        alternar_modo_viagem();
+    }
+    else if (strstr(*request, "GET /silenciar_alarme") != NULL)
+    {
+        silenciar_alarme();
+    }
+};
+
+// Função de callback para processar requisições HTTP
+static err_t tcp_server_recv(void *arg, struct tcp_pcb *tpcb, struct pbuf *p, err_t err)
+{
+    if (!p)
+    {
+        tcp_close(tpcb);
+        tcp_recv(tpcb, NULL);
+        return ERR_OK;
+    }
+
+    // Alocação do request na memória dinámica
+    char *request = (char *)malloc(p->len + 1);
+    memcpy(request, p->payload, p->len);
+    request[p->len] = '\0';
+
+    printf("Request: %s\n", request);
+
+    // Tratamento de request - Controle dos LEDs
+    user_request(&request);
+
+    // Leitura da temperatura interna
+    float temperature = temp_read();
+
+    // Cria a resposta HTML
+    char html[1024];
+    snprintf(html, sizeof(html),
+             "HTTP/1.1 200 OK\r\n"
+             "Content-Type: text/html\r\n\r\n"
+             "<html><head>"
+             "<style>body{background-color:#333;color:#fff;text-align:center}h1{margin:10px}button{width:150px;padding:10px;margin:5px;border:none;border-radius:6px;font-size:16px;color:#fff;cursor:pointer}.jumbo{padding:15px;background-color:#add8e6;border-radius:10px;margin:0 60%}</style>" // Azul claro para o jumbo
+             "</head><body>"
+             "<h1>Casa Inteligente</h1><div class='jumbo'>"
+
+             "<form action='/luz_1'><button style='background:blue'>&#x23FB;Luz</button></form>"
+             "<form action='/porta'><button style='background:#444'>&#x23FB;Porta</button></form>"
+
+             "<form action='/alarme'><button style='background:green'>&#x23FB;Alarme</button></form>"
+             "<form action='/modo_viagem'><button style='background:#444'>&#x23FB; Modo Viagem</button></form>"
+
+             "<form action='/luz_2'><button style='background:red'>&#x23FB; Luz 2</button></form>"
+             "<form action='/silenciar_alarme'><button style='background:#444'> Silenciar alarme </button></form>"
+
+             "<p>%.1f &deg;C</p>" // Temperatura dentro do "jumbo"
+             "</div></body></html>",
+             temperature);
+
+    // Escreve dados para envio (mas não os envia imediatamente).
+    tcp_write(tpcb, html, strlen(html), TCP_WRITE_FLAG_COPY);
+
+    // Envia a mensagem
+    tcp_output(tpcb);
+
+    // libera memória alocada dinamicamente
+    free(request);
+
+    // libera um buffer de pacote (pbuf) que foi alocado anteriormente
+    pbuf_free(p);
+
+    return ERR_OK;
+}
+
+// ---*-----------------------------------------------*------------------------------------------*--------
+
+// -------------------------------------- FUNÇÕES AUXILIARES DOS PERIFERICOS --------------------------------------
+
+#include "pico/bootrom.h"
+#define botaoB 6
+void gpio_irq_handler(uint gpio, uint32_t events)
+{
+    reset_usb_boot(0, 0);
+}
+
+void desenha_fig(uint32_t *_matriz, uint8_t _intensidade, PIO pio, uint sm) // FUNÇÃO PARA DESENHAR O SEMAFORO NA MATRIZ
+{
+    uint32_t pixel = 0;
+    uint8_t r, g, b;
+
+    for (int i = 24; i > 19; i--) // Linha 1
+    {
+        pixel = _matriz[i];
+        b = ((pixel >> 16) & 0xFF) * (_intensidade / 100.00); // Isola os 8 bits mais significativos (azul)
+        g = ((pixel >> 8) & 0xFF) * (_intensidade / 100.00);  // Isola os 8 bits intermediários (verde)
+        r = (pixel & 0xFF) * (_intensidade / 100.00);         // Isola os 8 bits menos significativos (vermelho)
+        pixel = 0;
+        pixel = (g << 16) | (r << 8) | b;
+        pio_sm_put_blocking(pio, sm, pixel << 8u);
+    }
+
+    for (int i = 15; i < 20; i++) // Linha 2
+    {
+        pixel = _matriz[i];
+        b = ((pixel >> 16) & 0xFF) * (_intensidade / 100.00); // Isola os 8 bits mais significativos (azul)
+        g = ((pixel >> 8) & 0xFF) * (_intensidade / 100.00);  // Isola os 8 bits intermediários (verde)
+        r = (pixel & 0xFF) * (_intensidade / 100.00);         // Isola os 8 bits menos significativos (vermelho)
+        pixel = 0;
+        pixel = (b << 16) | (r << 8) | g;
+        pixel = (g << 16) | (r << 8) | b;
+        pio_sm_put_blocking(pio, sm, pixel << 8u);
+    }
+
+    for (int i = 14; i > 9; i--) // Linha 3
+    {
+        pixel = _matriz[i];
+        b = ((pixel >> 16) & 0xFF) * (_intensidade / 100.00); // Isola os 8 bits mais significativos (azul)
+        g = ((pixel >> 8) & 0xFF) * (_intensidade / 100.00);  // Isola os 8 bits intermediários (verde)
+        r = (pixel & 0xFF) * (_intensidade / 100.00);         // Isola os 8 bits menos significativos (vermelho)
+        pixel = 0;
+        pixel = (g << 16) | (r << 8) | b;
+        pio_sm_put_blocking(pio, sm, pixel << 8u);
+    }
+
+    for (int i = 5; i < 10; i++) // Linha 4
+    {
+        pixel = _matriz[i];
+        b = ((pixel >> 16) & 0xFF) * (_intensidade / 100.00); // Isola os 8 bits mais significativos (azul)
+        g = ((pixel >> 8) & 0xFF) * (_intensidade / 100.00);  // Isola os 8 bits intermediários (verde)
+        r = (pixel & 0xFF) * (_intensidade / 100.00);         // Isola os 8 bits menos significativos (vermelho)
+        pixel = 0;
+        pixel = (g << 16) | (r << 8) | b;
+        pio_sm_put_blocking(pio, sm, pixel << 8u);
+    }
+
+    for (int i = 4; i > -1; i--) // Linha 5
+    {
+        pixel = _matriz[i];
+        b = ((pixel >> 16) & 0xFF) * (_intensidade / 100.00); // Isola os 8 bits mais significativos (azul)
+        g = ((pixel >> 8) & 0xFF) * (_intensidade / 100.00);  // Isola os 8 bits intermediários (verde)
+        r = (pixel & 0xFF) * (_intensidade / 100.00);         // Isola os 8 bits menos significativos (vermelho)
+        pixel = 0;
+        pixel = (g << 16) | (r << 8) | b;
+        pio_sm_put_blocking(pio, sm, pixel << 8u);
+    }
+}
+
+void draw_ssd1306(uint32_t *_matriz)
+{ // FUNÇÃO PARA DESENHAR NO DISPLAY COM O CÓDIGO EXPORTADO DO PISKEL
+    for (int i = 0; i < 8192; i++)
+    {
+        int x = i % 128; // coluna
+        int y = i / 128; // linha
+
+        if (_matriz[i] > 0x00000000)
+        {
+            ssd1306_pixel(&ssd, x, y, true);
+        }
+    }
+}
+
+void tocar_pwm_buzzer(uint duracao_ms)
+{
+    uint slice_num = pwm_gpio_to_slice_num(BUZZER_PIN);
+    pwm_set_enabled(slice_num, true);
+    sleep_ms(duracao_ms);
+    pwm_set_enabled(slice_num, false);
+}
+
+bool cor = true;
+
+// -------------------------------------- FUNÇÕES AUXILIARES DO SISTEMA --------------------------------------
+
+void atualizar_display()
+{
+    ssd1306_fill(&ssd, !cor);
+    ssd1306_rect(&ssd, 3, 3, 122, 60, cor, !cor);
+
+    // Desenhar imagem de porta conforme o estado
+    if (porta_open)
+    {
+        draw_ssd1306(porta_aberta);
+    }
+    else
+    {
+        draw_ssd1306(porta_fechada);
+    }
+
+    // PRIORIDADE 1: Alarme disparado (sobrepõe tudo, inclusive modo viagem)
+    if (alarme_disparado)
+    {
+        ssd1306_draw_string(&ssd, "ALERTA!", 56, 30);
+    }
+    // PRIORIDADE 2: Modo viagem (somente aparece se o alarme NÃO estiver disparado)
+    else if (modo_viagem)
+    {
+        ssd1306_draw_string(&ssd, "MODO", 65, 25);
+        ssd1306_draw_string(&ssd, "VIAGEM", 60, 35);
+    }
+    // PRIORIDADE 3: Estado normal do alarme ON/OFF
+    else
+    {
+        ssd1306_draw_string(&ssd, "ALARME", 65, 30);
+        ssd1306_draw_string(&ssd, alarme ? "ON" : "OFF", 75, 40);
+    }
+
+    ssd1306_send_data(&ssd);
+}
+
+// Leitura da temperatura interna
+float temp_read(void)
+{
+    adc_select_input(4);
+    uint16_t raw_value = adc_read();
+    const float conversion_factor = 3.3f / (1 << 12);
+    float temperature = 27.0f - ((raw_value * conversion_factor) - 0.706f) / 0.001721f;
+    return temperature;
+}
+
+// -------------------------------------- FUNÇÕES ACIONADA PELOS BOTÕES --------------------------------------
+
+void alternar_luz_1()
+{
+    luz_1 = !luz_1;
+
+    gpio_put(LED_BLUE_PIN, luz_1);
+    gpio_put(LED_RED_PIN, luz_1);
+    gpio_put(LED_GREEN_PIN, luz_1);
+}
+
+void alternar_luz_2()
+{
+    luz_2 = !luz_2;
+
+    if (luz_2)
+    {
+        desenha_fig(luz_quarto, BRILHO_PADRAO, pio, sm);
+    }
+    else
+    {
+        desenha_fig(matriz_apagada, BRILHO_PADRAO, pio, sm);
+    }
+}
+
+void alternar_porta()
+{
+    porta_open = !porta_open;
+    atualizar_display();
+}
+
+void alternar_alarme()
+{
+    if (!modo_viagem)
+    {
+        alarme = !alarme;
+    }
+
+    atualizar_display();
+}
+
+void silenciar_alarme()
+{
+    alarme_disparado = false;
+    modo_viagem = false;
+    alarme = false;
+    atualizar_display();
+}
+
+void alternar_modo_viagem()
+{
+    modo_viagem = !modo_viagem;
+
+    if (modo_viagem)
+    {
+        luz_1 = true;
+        luz_2 = true;
+        porta_open = true;
+        alarme = true;
+        alternar_luz_1();
+        alternar_luz_2();
+        alternar_porta();
+    }
+    else
+    {
+        alarme = false;
+        atualizar_display();
+    }
+}
+
+void monitorar()
+{
+    // Verifica se deve disparar o alarme
+    if ((alarme && porta_open) ||
+        (modo_viagem && (luz_1 || luz_2 || porta_open)))
+    {
+        alarme_disparado = true;
+    }
+
+    // Se o alarme foi silenciado, interrompe a disparação do alarme
+    if (alarme_disparado && !alarme && !modo_viagem)
+    {
+        alarme_disparado = false;
+    }
+    atualizar_display();
+    // Se o alarme foi disparado, mantém o buzzer tocando
+    if (alarme_disparado)
+    {
+        tocar_pwm_buzzer(800);
+    }
+}
---
+++ casa_inteligente.h
@@ -0,0 +1,128 @@
+#ifndef CASA_INTELIGENTE
+#define CASA_INTELIGENTE
+
+// ------------------------------
+// Bibliotecas padrão C
+// ------------------------------
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+// ------------------------------
+// SDK do Raspberry Pi Pico
+// ------------------------------
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "hardware/pwm.h"
+#include "hardware/adc.h"
+#include "hardware/i2c.h"
+#include "hardware/pio.h"
+#include "hardware/clocks.h"
+
+// ------------------------------
+// Conectividade Wi-Fi (CYW43)
+// ------------------------------
+#include "pico/cyw43_arch.h"
+#include "lwip/pbuf.h"
+#include "lwip/tcp.h"
+#include "lwip/netif.h"
+
+// ------------------------------
+// Bibliotecas externas do projeto
+// ------------------------------
+#include "lib/ssd1306.h"
+#include "lib/font.h"
+#include "lib/matriz_5X5.h"
+#include "lib/desenhos.h"
+#include "pio_wave.pio.h"
+
+// ------------------------------
+// Definições e constantes
+// ------------------------------
+
+// Credenciais da rede Wi-Fi
+#define WIFI_SSID       "BORGES"
+#define WIFI_PASSWORD   "gomugomu"
+
+// Pinos de LEDs
+#define LED_PIN         CYW43_WL_GPIO_LED_PIN
+#define LED_BLUE_PIN    12
+#define LED_GREEN_PIN   11
+#define LED_RED_PIN     13
+
+// Buzzer
+#define BUZZER_PIN      10
+
+// Botões
+#define botaoB          6
+
+// I2C - Display OLED
+#define I2C_PORT        i2c1
+#define I2C_SDA         14
+#define I2C_SCL         15
+#define endereco        0x3C
+
+// Matriz de LEDs
+#define MATRIZ_PIN      7
+#define NUM_PIXELS      25
+#define BRILHO_PADRAO   100
+
+// ------------------------------
+// Variáveis globais
+// ------------------------------
+PIO pio;             // Instância do PIO
+int sm;              // Máquina de estado
+ssd1306_t ssd;       // Instância do display OLED
+bool luz_1 = false;
+bool luz_2 = false;
+bool alarme = false;
+bool modo_viagem = false;
+bool porta_open = false;
+bool alarme_disparado = false;
+
+
+// ------------------------------
+// Protótipos de funções
+// ------------------------------
+
+// LEDs
+void gpio_led_bitdog(void);
+
+// PWM - Buzzer
+void inicializar_pwm_buzzer(void);
+void tocar_pwm_buzzer(uint duracao_ms);
+
+// Wi-Fi / TCP
+static err_t tcp_server_accept(void *arg, struct tcp_pcb *newpcb, err_t err);
+static err_t tcp_server_recv(void *arg, struct tcp_pcb *tpcb, struct pbuf *p, err_t err);
+
+// Sensor interno
+float temp_read(void);
+
+// Interface do usuário
+void user_request(char **request);
+
+// Display OLED
+void inicializar_display_i2c(void);
+void atualizar_display(void);
+void draw_ssd1306(uint32_t *_matriz);
+
+// Matriz de LEDs
+void configurar_matriz_leds(void);
+void desenha_fig(uint32_t *_matriz, uint8_t _intensidade, PIO pio, uint sm);
+void alternar_luz_1();
+void alternar_luz_2();
+void alternar_porta();
+void alternar_alarme();
+void monitorar();
+void silenciar_alarme();
+void alternar_modo_viagem();
+// GPIO / Interrupções
+void gpio_irq_handler(uint gpio, uint32_t events);
+void conectar_wifi();
+void configurar_servidor_tcp();
+void configurar_botao_bootsel();
+void inicializar_perifericos();
+void inicializar_sensor_temperatura();
+#endif // CASA_INTELIGENTE
